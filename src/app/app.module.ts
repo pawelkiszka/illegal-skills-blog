092950b24e2b388579f04600a9d3fb0c0cb5f500
Pawel Kiszka <pkiszka@outlook.com>
2019-02-23 13:00:56 +0100

    added prod build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import {
-    MatButtonModule,
-    MatCardModule,
-    MatChipsModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatSidenavModule,
-    MatToolbarModule
-} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HighlightModule } from 'ngx-highlightjs';
 import typescript from 'highlight.js/lib/languages/typescript';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreAppModule } from './core/core-app.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
+export function hljsLanguages() {
+    return [
+        {name: 'typescript', func: typescript},
+    ];
+}
 
 @NgModule({
     declarations: [
@@ -35,9 +26,9 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
         BrowserModule,
         BrowserAnimationsModule,
         HighlightModule.forRoot({
-            languages: () => [{name: 'typescript', func: typescript}]
+            languages: hljsLanguages
         }),
-        StoreDevtoolsModule.instrument(),
+        !environment.production ? StoreDevtoolsModule.instrument() : [],
     ],
     bootstrap: [AppComponent]
 })
