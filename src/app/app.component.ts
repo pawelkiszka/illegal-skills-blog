7a2722e8908fc7161d77c20aa299de6d608b3a67
Pawel Kiszka <pkiszka@outlook.com>
2019-05-04 11:56:41 +0200

    verify router events on refresh on prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+    constructor(private readonly router: Router){
+    }
+
+    public ngOnInit(): void {
+        this.router.events.subscribe(
+            event => console.log(event)
+        );
+    }
 }
