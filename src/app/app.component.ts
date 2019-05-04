71ec8c465984a067dcc367f45c758ad2ec2e5e2c
Pawel Kiszka <pkiszka@outlook.com>
2019-05-04 12:05:36 +0200

    Revert "verify router events on refresh on prod"
    This reverts commit 7a2722e8908fc7161d77c20aa299de6d608b3a67.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-
-    constructor(private readonly router: Router){
-    }
-
-    public ngOnInit(): void {
-        this.router.events.subscribe(
-            event => console.log(event)
-        );
-    }
+export class AppComponent {
 }
